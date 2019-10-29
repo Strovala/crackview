3a6da331bf9959fe75713d7a38e663b2f46da234
Strahinja Kovacevic <strahinja.kovacevic994@gmail.com>
2019-10-29 20:21:43 +0100

    Added exec commands

--- cmd/serve.go
+++ cmd/serve.go
@@ -1,18 +1,24 @@
 package cmd
 
 import (
+	"bytes"
 	"context"
 	"encoding/json"
 	"fmt"
+	"io/ioutil"
 	"log"
 	"net/http"
 	"os"
+	"os/exec"
 	"os/signal"
+	"runtime/debug"
+	"strings"
 	"syscall"
 	"time"
 
 	"github.com/go-chi/chi"
 	chiCors "github.com/go-chi/cors"
+	"github.com/pkg/errors"
 	"github.com/spf13/cobra"
 	"github.com/spf13/viper"
 )
@@ -26,8 +32,9 @@ func JSONRecoverer(next http.Handler) http.Handler {
 	fn := func(w http.ResponseWriter, r *http.Request) {
 		defer func() {
 			if rvr := recover(); rvr != nil {
-				log.Println("Panic occurred!", rvr)
-				JSONResponse(w, "Unexpected error occurred", 500)
+				fmt.Fprintf(os.Stderr, "Panic: %+v\n", rvr)
+				debug.PrintStack()
+				JSONResponse(w, "Unexpected error occurred", http.StatusInternalServerError)
 				return
 			}
 		}()
@@ -46,14 +53,204 @@ func serializeToString(data interface{}) (s string) {
 	return string(b)
 }
 
-// JSONResponse writes json response
-func JSONResponse(w http.ResponseWriter, data interface{}, httpCode int) {
+func writeJSONResponse(w http.ResponseWriter, data interface{}, httpCode int, key string) {
 	w.Header().Set("Content-Type", "application/json; charset=utf-8")
 	transformedData := map[string]interface{}{
-		"data": data,
+		key: data,
 	}
 	w.WriteHeader(httpCode)
 	fmt.Fprint(w, serializeToString(transformedData)+"\n")
+}
+
+// JSONResponse writes json response
+func JSONResponse(w http.ResponseWriter, data interface{}, httpCode int) {
+	writeJSONResponse(w, data, httpCode, "data")
+}
+
+// JSONErrorResponse writes json error response
+func JSONErrorResponse(w http.ResponseWriter, data interface{}, httpCode int) {
+	writeJSONResponse(w, data, httpCode, "error")
+}
+
+// Unmarshal deserializes request body
+func Unmarshal(dest interface{}, r *http.Request) (err error) {
+	body, err := ioutil.ReadAll(r.Body)
+	if err != nil {
+		return err
+	}
+	defer func() {
+		// do not assign to err directly since this is executing last and
+		// we might set it to nil when someone after is setting it to non-nil
+		closeErr := r.Body.Close()
+		if closeErr != nil {
+			err = closeErr
+		}
+	}()
+
+	err = json.Unmarshal(body, dest)
+	if err, ok := err.(*json.SyntaxError); ok && err != nil {
+		return errors.New("Unable to parse JSON body")
+	}
+	if err, ok := err.(*json.UnmarshalTypeError); ok && err != nil {
+		return errors.New("Cannot unmarshal JSON body into type")
+	}
+	return err
+}
+
+func homeHandler(w http.ResponseWriter, r *http.Request) error {
+	JSONResponse(w, fmt.Sprintf("Of languages we support: %v", viper.GetStringSlice("languages")), http.StatusOK)
+	return nil
+}
+
+// CodeRequest is DTO for request for code run
+type CodeRequest struct {
+	Text string `json:"text"`
+	Lang string `json:"lang"`
+}
+
+// CodeResponse is DTO for response for code run
+type CodeResponse struct {
+	Output string `json:"output"`
+	Error  string `json:"error"`
+}
+
+func execPython(text string) (*CodeResponse, error) {
+	err := ioutil.WriteFile("main.py", []byte(text), 0644)
+	if err != nil {
+		return nil, err
+	}
+	cmd := exec.Command("python3", "main.py")
+	var out bytes.Buffer
+	var errOut bytes.Buffer
+	cmd.Stdout = &out
+	cmd.Stderr = &errOut
+	_ = cmd.Run()
+	err = os.Remove("main.py")
+	if err != nil {
+		return nil, err
+	}
+	resp := &CodeResponse{
+		Output: out.String(),
+		Error:  errOut.String(),
+	}
+	return resp, nil
+}
+
+func execCpp(text string) (*CodeResponse, error) {
+	err := ioutil.WriteFile("main.cpp", []byte(text), 0644)
+	if err != nil {
+		return nil, err
+	}
+	cmd := exec.Command("c++", "-o", "main", "main.cpp")
+	var out bytes.Buffer
+	var errOut bytes.Buffer
+	cmd.Stdout = &out
+	cmd.Stderr = &errOut
+	_ = cmd.Run()
+	if errOut.Len() != 0 {
+		err = os.Remove("main.cpp")
+		if err != nil {
+			return nil, err
+		}
+		return &CodeResponse{
+			Error: errOut.String(),
+		}, nil
+	}
+	cmd = exec.Command("./main")
+	cmd.Stdout = &out
+	cmd.Stderr = &errOut
+	_ = cmd.Run()
+	err = os.Remove("main.cpp")
+	err = os.Remove("main")
+
+	return &CodeResponse{
+		Output: out.String(),
+	}, nil
+}
+
+func execJava(text string) (*CodeResponse, error) {
+	err := ioutil.WriteFile("main.java", []byte(text), 0644)
+	if err != nil {
+		return nil, err
+	}
+	cmd := exec.Command("javac", "main.java")
+	var out bytes.Buffer
+	var errOut bytes.Buffer
+	cmd.Stdout = &out
+	cmd.Stderr = &errOut
+	_ = cmd.Run()
+	if errOut.Len() != 0 {
+		err = os.Remove("main.java")
+		if err != nil {
+			return nil, err
+		}
+		return &CodeResponse{
+			Error: errOut.String(),
+		}, nil
+	}
+	files, err := ioutil.ReadDir("./")
+	if err != nil {
+		return nil, err
+	}
+
+	unparsedExecName := ""
+	for _, file := range files {
+		if strings.Contains(file.Name(), ".class") {
+			unparsedExecName = file.Name()
+			break
+		}
+	}
+	execName := strings.Split(unparsedExecName, ".")[0]
+	cmd = exec.Command("java", execName)
+	cmd.Stdout = &out
+	cmd.Stderr = &errOut
+	_ = cmd.Run()
+	err = os.Remove("main.java")
+	err = os.Remove(unparsedExecName)
+
+	return &CodeResponse{
+		Output: out.String(),
+	}, nil
+}
+
+const (
+	PYTHON = "python"
+	JAVA   = "java"
+	CPP    = "c++"
+)
+
+func homeHandlerPost(w http.ResponseWriter, r *http.Request) error {
+	var data CodeRequest
+	err := Unmarshal(&data, r)
+	if err != nil {
+		return err
+	}
+
+	var execFunc func(string) (*CodeResponse, error)
+	switch data.Lang {
+	case PYTHON:
+		execFunc = execPython
+	case JAVA:
+		execFunc = execJava
+	case CPP:
+		execFunc = execCpp
+	}
+	resp, err := execFunc(data.Text)
+	if err != nil {
+		return err
+	}
+
+	JSONResponse(w, *resp, http.StatusOK)
+	return nil
+}
+
+func errorHandler(next func(http.ResponseWriter, *http.Request) error) func(w http.ResponseWriter, r *http.Request) {
+	return func(w http.ResponseWriter, r *http.Request) {
+		err := next(w, r)
+		if err != nil {
+			JSONErrorResponse(w, err.Error(), 500)
+		}
+	}
 }
 
 func makeServer() *http.Server {
@@ -69,14 +266,15 @@ func makeServer() *http.Server {
 	router.Use(cors.Handler)
 	router.Use(JSONRecoverer)
 
-	router.Get("/", func(w http.ResponseWriter, r *http.Request) {
-		JSONResponse(w, "blant", 200)
-	})
+	router.Get("/", errorHandler(homeHandler))
+	router.Post("/", errorHandler(homeHandlerPost))
 
 	s := &http.Server{
-		Addr:        fmt.Sprintf(":%v", viper.GetString("port")),
-		Handler:     router,
-		ReadTimeout: 2 * time.Minute,
+		Addr:         fmt.Sprintf(":%v", viper.GetString("port")),
+		Handler:      router,
+		ReadTimeout:  5 * time.Second,
+		WriteTimeout: 10 * time.Second,
+		IdleTimeout:  120 * time.Second,
 	}
 
 	return s
--- go.mod
+++ go.mod
@@ -5,8 +5,10 @@ go 1.12
 require (
 	github.com/go-chi/chi v4.0.2+incompatible
 	github.com/go-chi/cors v1.0.0
+	github.com/pkg/errors v0.8.0
 	github.com/spf13/cobra v0.0.5
 	github.com/spf13/viper v1.4.0
 	github.com/stretchr/testify v1.3.0 // indirect
+	go.uber.org/zap v1.10.0
 	golang.org/x/text v0.3.1-0.20180807135948-17ff2d5776d2 // indirect
 )
